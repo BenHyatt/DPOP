57c39264463dd14b38c95dfbae7d5930e2798789
Ben Underwood <benu423@gmail.com>
2019-01-19 17:45:46 -0500

    Create export.java

---
+++ export.java
@@ -0,0 +1,259 @@
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class export {
+	int[][] dpop;
+	String[][] export;
+	String[] volNames;
+	String fileName;
+
+	public export(int[][] schedule, String[] names, String file) {
+		dpop = schedule;
+		export = new String[dpop.length + 1][dpop[0].length];
+		volNames = names;
+		fileName = file;
+	}
+
+	public void process() {
+		// Add verticle lines for repeated activities
+		for (int i = 0; i < dpop.length; i++) {
+			for (int a = 0; a < dpop[0].length; a++) {
+				if (i > 1 && i % 2 == 1 && dpop[i - 1][a] == dpop[i][a] && dpop[i][a] != 0) {
+					export[i][a] = "|";
+				} else {
+					export[i][a] = getName(dpop[i][a]);
+				}
+			}
+		}
+
+		// Add xxxx to bottom of dpop
+		for (int i = 0; i < export[0].length; i++) {
+			export[export.length - 1][i] = "xxxx";
+		}
+
+		// Add times to when people stop
+		for (int i = 0; i < export[0].length; i++) {
+			for (int a = 0; a < export.length - 1; a++) {
+				if (!export[a][i].equals("xxxx") && export[a + 1][i].equals("xxxx")) {
+					export[a + 1][i] = clockTime(a + 1);
+					break;
+				}
+			}
+		}
+
+		// Remove the "xxxx"
+		for (int i = 0; i < export[0].length; i++) {
+			for (int a = 0; a < export.length; a++) {
+				if (export[a][i].equals("xxxx")) {
+					export[a][i] = " ";
+				}
+			}
+		}
+	}
+
+	public void toExcel() {
+		Workbook wb;
+		wb = new HSSFWorkbook();
+		Sheet sheet = wb.createSheet("DPOP");
+
+		CellStyle cellStyle = wb.createCellStyle();
+		cellStyle.setAlignment(HorizontalAlignment.CENTER);
+
+		CellStyle atrium = wb.createCellStyle();
+		atrium.setAlignment(HorizontalAlignment.CENTER);
+		atrium.setFillForegroundColor(IndexedColors.LAVENDER.getIndex());
+		atrium.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+		CellStyle planet = wb.createCellStyle();
+		planet.setAlignment(HorizontalAlignment.CENTER);
+		planet.setFillForegroundColor(IndexedColors.YELLOW.getIndex());
+		planet.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+		CellStyle movie = wb.createCellStyle();
+		movie.setAlignment(HorizontalAlignment.CENTER);
+		movie.setFillForegroundColor(IndexedColors.GREEN.getIndex());
+		movie.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+		CellStyle dinos = wb.createCellStyle();
+		dinos.setAlignment(HorizontalAlignment.CENTER);
+		dinos.setFillForegroundColor(IndexedColors.DARK_RED.getIndex());
+		dinos.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+		CellStyle lunch = wb.createCellStyle();
+		lunch.setAlignment(HorizontalAlignment.CENTER);
+		lunch.setFillForegroundColor(IndexedColors.BLUE.getIndex());
+		lunch.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+		CellStyle greet = wb.createCellStyle();
+		greet.setAlignment(HorizontalAlignment.CENTER);
+		greet.setFillForegroundColor(IndexedColors.LIGHT_TURQUOISE.getIndex());
+		greet.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+		Row row;
+		Cell cell;
+
+		row = sheet.createRow(0);
+		cell = row.createCell(1);
+		cell.setCellStyle(cellStyle);
+		cell.setCellValue("Guest Services Volunteers");
+
+		// sheet.addMergedRegion(new CellRangeAddress(0,0,1,3));
+
+		cell = row.createCell(3);
+		cell.setCellStyle(cellStyle);
+		Date now = new Date();
+		SimpleDateFormat formatter = new SimpleDateFormat("MMM-d");
+		cell.setCellValue(formatter.format(now));
+
+		cell = row.createCell(4);
+		cell.setCellStyle(cellStyle);
+		formatter = new SimpleDateFormat("EEEEEEEEEEEE");
+		cell.setCellValue(formatter.format(now));
+
+		row = sheet.createRow(1);
+		cell = row.createCell(0);
+		cell.setCellStyle(cellStyle);
+		cell.setCellValue("Name");
+		for (int i = 0; i < volNames.length; i++) {
+			cell = row.createCell(i + 1);
+			cell.setCellStyle(cellStyle);
+			cell.setCellValue(volNames[i]);
+		}
+
+		row = sheet.createRow(2);
+		cell = row.createCell(0);
+		cell.setCellStyle(cellStyle);
+		cell.setCellValue("Shift");
+
+		row = sheet.createRow(3);
+		cell = row.createCell(0);
+		cell.setCellStyle(cellStyle);
+		cell.setCellValue("Notes");
+
+		String activityName = "";
+		int rownum = 4;
+		for (int i = 0; i < export.length; i++, rownum++) {
+			row = sheet.createRow(rownum);
+			cell = row.createCell(0);
+			cell.setCellStyle(cellStyle);
+			cell.setCellValue(clockTime(i));
+			for (int j = 0; j < export[i].length; j++) {
+
+				cell = row.createCell(j + 1);
+				cell.setCellValue(export[i][j]);
+
+				if (export[i][j].equals("|")) {
+					activityName = export[i - 1][j];
+				} else {
+					activityName = export[i][j];
+				}
+
+				cell.setCellStyle(cellStyle);
+				if (activityName.equals("Planet"))
+					cell.setCellStyle(planet);
+				if (activityName.equals("Movie"))
+					cell.setCellStyle(movie);
+				if (activityName.equals("Atrium"))
+					cell.setCellStyle(atrium);
+				if (activityName.equals("Dinos"))
+					cell.setCellStyle(dinos);
+				if (activityName.equals("Greet"))
+					cell.setCellStyle(greet);
+				if (activityName.equals("Lunch"))
+					cell.setCellStyle(lunch);
+
+			}
+			cell = row.createCell(export[i].length + 1);
+			cell.setCellStyle(cellStyle);
+			cell.setCellValue(clockTime(i));
+		}
+		File file = new File(fileName);
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				System.out.println("ERROR trying to create file");
+			}
+		}
+		try (FileOutputStream out = new FileOutputStream(file)) {
+			System.out.println("OUTPUTING FILE");
+			wb.write(out);
+		} catch (IOException e) {
+			System.out.println("ERROR Trying to write");
+		}
+		try {
+			wb.close();
+		} catch (IOException e) {
+			System.out.println("ERROR TRING TO CLOSE");
+		}
+	}
+
+	public String getName(int volunteerAssignment) {
+		String result = "";
+		if (volunteerAssignment == 0) {
+			result += "xxxx";
+		}
+		if (volunteerAssignment == 1) {
+			result += "------";
+		}
+		if (volunteerAssignment == 2) {
+			result += "Lunch";
+		}
+		if (volunteerAssignment == 3) {
+			result += "Movie";
+		}
+		if (volunteerAssignment == 4) {
+			result += "Planet";
+		}
+		if (volunteerAssignment == 5) {
+			result += "Dinos";
+		}
+		if (volunteerAssignment == 6) {
+			result += "Atrium";
+		}
+		if (volunteerAssignment == 7) {
+			result += "STARS";
+		}
+		if (volunteerAssignment == 8) {
+			result += "Meeting";
+		}
+		if (volunteerAssignment == 9) {
+			result += "Greet";
+		}
+		if (volunteerAssignment == 10) {
+			result += "STARS";
+		}
+		if (volunteerAssignment == 11) {
+			result += "Rove";
+		}
+		if (volunteerAssignment == 12) {
+			result += "Load Movie";
+		}
+		if (volunteerAssignment == 13) {
+			result += "Load Planet";
+		}
+		return result;
+	}
+
+	public String clockTime(int i) {
+		String time = "";
+		double k;
+		if (i * .5 + 9 > 12.5) {
+			k = (i * .5 + 9) - 12;
+		} else {
+			k = (i * .5 + 9);
+		}
+		if (k % 1 != 0) {
+			time = Math.round(Math.floor(k)) + ":30";
+		} else {
+			time = Math.round(Math.floor(k)) + ":00";
+		}
+		return time;
+
+	}
+}
